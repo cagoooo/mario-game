0cf5dc914d4f37e9953353be3b01b775ef29518c
cagoooo <ipad@mail2.smes.tyc.edu.tw>
2025-12-11 19:45:30 +0800

    Force reload of Game.js with version tag

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { Game } from './Game.js';
+import { Game } from './Game.js?v=1.1.3';
 
 window.onload = function () {
-    console.log('Game Version: 1.1.2 (Pipe Fix)');
+    console.log('Game Version: 1.1.3 (Cache Busting)');
     const canvas = document.getElementById('gameArea');
 
     // UI Elements
