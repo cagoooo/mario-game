9db0f46156adf6a747c1624f30796b57f48e2f86
cagoooo <ipad@mail2.smes.tyc.edu.tw>
2025-12-14 16:18:39 +0800

    Release v1.9.6: Fix Landscape Aspect Ratio (v6)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { Game, preloadImages } from './Game.js?v=1.9.5';
+import { Game, preloadImages } from './Game.js?v=1.9.6';
 
 window.onload = async function () {
-    console.log('%c Game Version: 1.9.5 (Fix: Landscape Aspect Ratio v5) ', 'background: #222; color: #00ff00; font-size: 20px; padding: 10px;');
+    console.log('%c Game Version: 1.9.6 (Fix: Landscape Aspect Ratio v6) ', 'background: #222; color: #00ff00; font-size: 20px; padding: 10px;');
     const canvas = document.getElementById('gameArea');
 
     // UI Elements
